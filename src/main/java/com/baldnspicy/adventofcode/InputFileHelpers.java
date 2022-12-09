347e6fd24c0c4eb4c1e0c4ca74e825e53d22c547
Wes Sorensen <wsorensen@clearwateranalytics.com>
2022-12-09 13:56:24 -0700

    2022 - Tweaks to day 1, added file helpers class

---
+++ src/main/java/com/baldnspicy/adventofcode/InputFileHelpers.java
@@ -0,0 +1,22 @@
+package com.baldnspicy.adventofcode;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class InputFileHelpers {
+
+    /** Takes day as input, returns List of strings from that day's input */
+    public static List<String> getStringList(int day) {
+        InputStream puzzleInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("2022/day" + day);
+        List<String> strings = null;
+        if (puzzleInputStream == null) {
+            System.err.println("Missing input data; expected to find 2022/day" + day);
+        } else {
+            strings = new BufferedReader(new InputStreamReader(puzzleInputStream))
+                    .lines().toList();
+        }
+        return strings;
+    }
+}
--- src/main/java/com/baldnspicy/adventofcode/aoc2022/Day1.java
+++ src/main/java/com/baldnspicy/adventofcode/aoc2022/Day1.java
@@ -1,4 +1,71 @@
 package com.baldnspicy.adventofcode.aoc2022;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import static com.baldnspicy.adventofcode.InputFileHelpers.getStringList;
+
 public class Day1 {
+
+    public static void main(String[] args) {
+        List<String> inputStrings;
+        int day = 1;
+
+        inputStrings = getStringList(day);
+
+        partOne(inputStrings);
+        partTwo(inputStrings);
+    }
+
+    private static void partOne(List<String> strs) {
+        List<Integer> elves = new ArrayList<>();
+        int sum = 0;
+        int largest = 0;
+        for (String line : strs) {
+            if (Objects.equals(line, "")) {
+                elves.add(sum);
+                if (sum > largest) largest = sum;
+                sum = 0;
+                continue;
+            }
+            sum += Integer.parseInt(line);
+        }
+        if (sum > largest) largest = sum;
+        elves.add(sum);  // add in last guy
+        System.out.println(elves);
+        System.out.println(largest);
+    }
+
+    private static void partTwo(List<String> strs) {
+        List<Integer> elves = new ArrayList<>();
+        int sum = 0;
+        int largest = 0;
+        int nextLargest = 0;
+        int nextNextLargest = 0;
+        for (String line : strs) {
+            if (Objects.equals(line, "")) {
+                elves.add(sum);
+                if (sum > largest) {
+                    largest = sum;
+                } else if (sum > nextLargest) {
+                    nextLargest = sum;
+                } else if (sum > nextNextLargest) {
+                    nextNextLargest = sum;
+                }
+                sum = 0;
+                continue;
+            }
+            sum += Integer.parseInt(line);
+        }
+//        if (sum > largest) largest = sum;
+        elves.add(sum);  // add in last guy
+
+        Collections.sort(elves);
+        int len = elves.size();
+        int threeElves = elves.get(len-1) + elves.get(len-2) + elves.get(len-3);
+        System.out.println(elves);
+        System.out.println(threeElves);
+    }
 }
