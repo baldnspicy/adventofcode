2e924b7f796f03e87f0323964b2c668b1e6c607e
Wes Sorensen <wsorensen@clearwateranalytics.com>
2022-12-16 16:36:57 -0700

    2022 - Day 6

---
+++ src/main/java/com/baldnspicy/adventofcode/aoc2022/Day6.java
@@ -0,0 +1,49 @@
+package com.baldnspicy.adventofcode.aoc2022;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import static com.baldnspicy.adventofcode.InputFileHelpers.getStringList;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        List<String> inputStrings;
+
+        String day = "6";
+        inputStrings = getStringList(day);
+        partOne(inputStrings.get(0));  // Answer: 1100
+
+        String day2 = "6";
+        inputStrings = getStringList(day2);
+        partTwo(inputStrings.get(0));  // Answer: 2421
+    }
+
+    static void partOne(String line) {
+        System.out.println("*** Part 1 answer: " + findSubstring(line, 4));
+    }
+
+    static void partTwo(String line) {
+        System.out.println("*** Part 2 answer: " + findSubstring(line, 14));
+    }
+
+    static int findSubstring(String line, int substringSize) {
+        Set<Character> subStrChars;
+        String subStr;
+        for (int i = 0; i < line.length(); i++) {
+            subStrChars = new HashSet<>();
+            subStr = line.substring(i, i+substringSize);
+            for (int j = 0; j < substringSize; j++) {
+                subStrChars.add(subStr.charAt(j));
+            }
+            if (subStrChars.size() == substringSize) {
+                return (i + substringSize);
+            }
+        }
+        return 0;
+    }
+
+}
