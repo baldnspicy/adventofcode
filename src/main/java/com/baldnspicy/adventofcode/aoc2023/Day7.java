44fb92a2e1025181435811705a9ce3015c109a7b
Wes Sorensen <wsorensen@clearwateranalytics.com>
2024-12-02 08:51:56 -0700

    2023 - pushing whatever I have as I begin 2024

---
+++ src/main/java/com/baldnspicy/adventofcode/aoc2023/Day7.java
@@ -0,0 +1,62 @@
+package com.baldnspicy.adventofcode.aoc2023;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.baldnspicy.adventofcode.InputFileHelpers.getStringList;
+
+public class Day7 {
+    enum PlayingCard {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
+//    public static void main(String[] args) {
+//        List<String> inputStrings;
+//        String day = "7";
+//
+//        inputStrings = getStringList("2023", day);
+//
+//        partOne(inputStrings);
+//        partTwo(inputStrings);
+//    }
+//
+//    public static void partOne(List<String> inputStrings) {
+//        Map<String, Integer> hands = parseHands(inputStrings);
+//        hands = sortHands(hands);
+//    }
+//
+//    public static Map<String, Integer> parseHands(List<String> inputStrings) {
+//        Map<String, Integer> hands = new HashMap<>();
+//        for (String line : inputStrings) {
+//            String[] parts = line.split(" ");
+//            hands.put(parts[0], Integer.parseInt(parts[1]));
+//        }
+//        return hands;
+//    }
+//
+//    public static Map<String, Integer> sortHands(Map<String, Integer> hands) {
+//        int aces = 0;
+//        int king = 0;
+//        int queen = 0;
+//        int jack = 0;
+//        int ten = 0;
+//        int nine = 0;
+//        int eight = 0;
+//        int seven = 0;
+//        int six = 0;
+//        int five = 0;
+//        int four = 0;
+//        int three = 0;
+//        int two = 0;
+//
+//
+//        Map<String, Integer> sortedHands = new HashMap<>();
+//
+//    }
+//
+//    public static boolean isFiveInRow(String hand) {
+////        return hand.contains()
+//    }
+//
+//    public static void partTwo(List<String> inputStrings) {
+//
+//    }
+}
---
+++ src/main/java/com/baldnspicy/adventofcode/aoc2023/Day9.java
@@ -0,0 +1,26 @@
+package com.baldnspicy.adventofcode.aoc2023;
+
+import java.util.List;
+
+import static com.baldnspicy.adventofcode.InputFileHelpers.getStringList;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+        List<String> inputStrings;
+        String day = "9";
+
+        inputStrings = getStringList("2023", day);
+
+        partOne(inputStrings);
+        partTwo(inputStrings);
+    }
+
+    public static void partOne(List<String> inputStrings) {
+
+    }
+
+    public static void partTwo(List<String> inputStrings) {
+
+    }
+}
